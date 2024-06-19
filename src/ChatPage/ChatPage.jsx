0e413e53dd59f4a4408b05df21c91b9e16ff9bc2
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-06-19 10:43:28 +0900

    chat Count 추가

--- src/ChatPage/ChatPage.jsx
+++ src/ChatPage/ChatPage.jsx
@@ -8,24 +8,34 @@ import { animateScroll as scroll } from "react-scroll";
 
 const ChatPage = () => {
   const { id } = useParams();
+  console.log("통합 URI :: ",id);
+
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   const [nickname, setNickname] = useState('');
   const [stompClient, setStompClient] = useState(null);
+  const [userId, setUserid] = useState(''); // user id 
+  const [chatRoomId, setChatRoomId] = useState(''); // chat room id
 
-  const url = `http://175.45.200.47`;/// 배포용
-  // const url = `http://localhost`;
-
+  // const url = `http://175.45.200.47`;/// 배포용
+  const url = `http://localhost:8080`;
   const scrollContainerRef = useRef(null);
 
   useEffect(() => {
+    const idParts = id.split("-");
+    const partsCombined = idParts.slice(0, 5).join("-");
+    const userName = atob(idParts[5]);
+    setChatRoomId(partsCombined);
+    setUserid(userName);
+    console.log("userName : ", userName);
+    console.log("채팅방 URL",partsCombined)
     const socket = new SockJS(`${url}/ws`);
     const client = Stomp.over(socket);
 
     client.connect({}, (frame) => {
       console.log('Connected: ' + frame);
-      console.log("SUBSCRIBE : ",`/topic/messages/${id}`)
-      client.subscribe(`/topic/messages/${id}`, (message) => {
+      console.log("SUBSCRIBE : ",`/topic/messages/${chatRoomId}`)
+      client.subscribe(`/topic/messages/${chatRoomId}`, (message) => {
         const receivedMessage = JSON.parse(message.body);
         console.log("receive : ",receivedMessage)
         setMessages((prevMessages) => [...prevMessages, receivedMessage]);
@@ -35,7 +45,7 @@ const ChatPage = () => {
       console.error("STOMP ERROR", error);
     })
 
-    axios.get(`${url}/chat/history?id=${id}`)
+    axios.get(`${url}/chat/history?id=${partsCombined}&user=${userName}`)
       .then((response) => {
         setMessages(response.data);
       })
@@ -65,15 +75,20 @@ const ChatPage = () => {
     setNickname(e.target.value);
     console.log(nickname);
   }
+  const handlerKeyDown= (e) => {
+    if(e.key === 'Enter' && message.trim()){
+      sendMessage();
+    }
+  }
   const handlerMessageChange = (e) => {
     setMessage(e.target.value);
   }
-  const sendMessage = () => {
+  const sendMessage = () => { 
     console.log("SEND MESSAGE",nickname);
     if(message.trim() && stompClient && stompClient.connected){
       const chatMessage = {
-        id : id,
-        name: nickname,
+        id : chatRoomId,
+        name: userId,
         content: message
       }
       console.log("IS OK");
@@ -89,14 +104,14 @@ const ChatPage = () => {
         <div>
         {messages.map((msg, index) => {
           const style = {
-            textAlign : msg.name === "김승원" ? "right" : "left"
+            textAlign : msg.name === userId ? "right" : "left"
           };
           return(
             <div key={index} style={style} >
-              {msg.name === "김승원" ? (
+              {msg.name === userId ? (
                 <div className="chat chat-end">
                   <div className={`chat-bubble text-white bg-blue-500`}>
-                    {msg.name}: {msg.content}
+                    {msg.content} : {msg.name}
                   </div>
                   <div>{msg.timeStamp}</div>
                 </div>
@@ -117,12 +132,8 @@ const ChatPage = () => {
       <br/>
       <br/>
       <div ref={scrollContainerRef} className="fixed bottom-2 w-full">
-        <div>
-          <label>사용자 이름: </label>
-          <input type="text" value={nickname} onChange={handlerNickNameChange} className="border text-sm rounded-lg block w-full p-2.5 bg-gray-300 border-gray-"/>
-        </div>
         <span className="w-full grid">
-          <input type="text" value={message} onChange={handlerMessageChange} className="border text-sm rounded-lg block w-full p-2.5 bg-gray-300 border-gray-"/>
+          <input type="text" value={message} onChange={handlerMessageChange} onKeyDown={handlerKeyDown} className="border text-sm rounded-lg block w-full p-2.5 bg-gray-300 border-gray-"/>
           <button onClick={sendMessage} className='btn btn-info'>보내기</button>
         </span>
       </div>
