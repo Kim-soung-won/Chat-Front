553cccb8c335cb843f484b6687f3f0f81cfc709a
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-06-20 19:36:41 +0900

    header 교체

--- src/DefaultLayouts/Header.jsx
+++ src/DefaultLayouts/Header.jsx
@@ -1,100 +1,107 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import SearchIcon from '@mui/icons-material/Search';
-import {useEffect, useState} from 'react';
-import {CssBaseline} from "@mui/material";
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import Swal from 'sweetalert2';
-import {EventSourcePolyfill} from "event-source-polyfill";
+import {CssBaseline, Typography, useMediaQuery} from "@mui/material";
+import {Link, useNavigate} from 'react-router-dom';
+import "../App.css";
+import {useTheme} from "@mui/material/styles";
 
 export default function Header() {
 
-    const [notification, setNotification] = useState(); // 최근 알림 저장
-    const [eventSource, setEventSource] = useState(null);
+    // const [notification, setNotification] = useState(); // 최근 알림 저장
+    // const [eventSource, setEventSource] = useState(null);
 
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-    })
+    const theme = useTheme();
+    const isMobileView = useMediaQuery(theme.breakpoints.down('sm'));
 
-    // 실시간 알림 SSE 요청
-    useEffect(() => {
+    // const Toast = Swal.mixin({
+    //     toast: true,
+    //     position: 'top-end',
+    //     showConfirmButton: false,
+    //     timer: 2000,
+    //     timerProgressBar: true,
+    //     didOpen: (toast) => {
+    //         toast.addEventListener('mouseenter', Swal.stopTimer)
+    //         toast.addEventListener('mouseleave', Swal.resumeTimer)
+    //     },
+    //     customClass: {
+    //         container: 'toastContainer',
+    //     }
+    // })
 
-        const intervalId = setInterval(() => {
-            if (eventSource && eventSource.readyState === EventSource.CLOSED) {
-                console.log('SSE connection closed, reconnecting...');
-            }
-        }, 5000);
 
-        return () => {
-            clearInterval(intervalId);
-            if (eventSource) {
-                eventSource.close();
-            }
-        };
+    // // 실시간 알림 SSE 요청
+    // useEffect(() => {
 
-    }, []);
+    //     const intervalId = setInterval(() => {
+    //         if (eventSource && eventSource.readyState === EventSource.CLOSED) {
+    //             console.log('SSE connection closed, reconnecting...');
+    //         }
+    //     }, 5000);
+
+    //     return () => {
+    //         clearInterval(intervalId);
+    //         if (eventSource) {
+    //             eventSource.close();
+    //         }
+    //     };
+
+    // }, []);
 
     // 알림 리스트
-    useEffect(() => {
-        if (notification) {
-            console.log("notification data", notification);
-        }
-    }, [notification]);
+    // useEffect(() => {
+    //     if (notification) {
+    //         console.log("notification data", notification);
+    //     }
+    // }, [notification]);
 
     // 로그아웃 요청 api
 
 
 
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-        return (
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <CssBaseline />
+            <AppBar
+                sx={{
+                    position: 'fixed',
+                    bottom: '10',
+                    left: 0,
+                    right: 0,
+                    backgroundColor: 'white',
+                    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.05)',
+                }}
+            >
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{
+                            mr: 2,
+                            flexShrink: 0,
+                            fontSize: isMobileView ? '1rem' : '1.25rem',
+                            color: 'black',
+                        }}
+                    >
+                        <a href="http://175.106.99.78" style={{ color: 'black', textDecoration: 'none' }}>
+                            냥가왈부
+                        </a>
+                    </Typography>
 
-            <Box sx={{flexGrow: 1}}>
-                <CssBaseline/>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <Link to="/" style={{color: '#fff'}}>냥가왈부</Link>
-
-                        <Box sx={{flexGrow: 1}}/>
-                        <Box sx={{display: {xs: 'none', md: 'flex'}}}>
-                            <IconButton size="large" color="inherit">
-                                <Badge>
-                                    <SearchIcon/>
-                                </Badge>
-                            </IconButton>
-                        </Box>
-                        <Box sx={{display: {xs: 'flex', md: 'none'}}}>
-                            <IconButton
-                                size="large"
-                                aria-label="show more"
-                                aria-controls={mobileMenuId}
-                                aria-haspopup="true"
-                                color="inherit"
-                            >
-                                <MoreIcon/>
-                            </IconButton>
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-
-        );
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: isMobileView ? 'flex-end' : 'flex-end',
+                            width: isMobileView ? '100%' : 'auto',
+                            pr: isMobileView ? 2 : 0,
+                        }}
+                    >
+                        
+                        
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
