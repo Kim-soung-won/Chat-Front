de4d01a3df8a7830143f8b7b02dfd930b3ad44a9
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-06-21 18:42:27 +0900

    chat img style 수정

--- src/ChatPage/ChatPage.jsx
+++ src/ChatPage/ChatPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Stomp from 'stompjs';
 import SockJS from "sockjs-client";
 import axios from "axios";
-import "../App.css";
+import Apps from "../App.css";
 import { animateScroll as scroll } from "react-scroll";
 
 const ChatPage = () => {
@@ -120,7 +120,7 @@ const ChatPage = () => {
               ) : (
               <div className="chat chat-start">
                 <img src={`https://kr.object.ncloudstorage.com/palettepets/member/Profile/20220509173224_d9N4ZGtBVR.jpeg` }
-                className={receverImg} ></img>
+                className={Apps.receverImg} ></img>
                 <div className="flex">
                   <div className={`chat-bubble text-black bg-slate-100`}>
                     {msg.name} : {msg.content}
